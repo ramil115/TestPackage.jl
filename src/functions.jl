e38a910367750d8189b8b650333606e5cd158b4c
Ramil Khusainov <r.khusainov@innopolis.ru>
2023-05-30 12:58:57 +0300

    modifications accroding to comments

--- src/functions.jl
+++ src/functions.jl
@@ -15,26 +15,66 @@ end
 
 
 """
-    random_fibonachi_triple()
-Generate array of three consequtive Fibonacchi numbers.
-# Examples
+    fibonacci_sequence(sequence_length_array::Array{Int}, number_of_cycles::Integer, get_even::Bool)
+Generate arrays of consequtive Fibonacci numbers.
+# Arguments
+- `sequence_length_array::Array{Int}`: array of desired sequence lengths. Default value is 3.
+- `array_number::Integer`: total number of output sequences. Default value is 5.
+- `get_even::Bool`: set True if even members of Fibonacci series needed. Default value is False.
 ```julia-repl
-julia> random_fibonachi_triple()
-[6765, 10946, 17711]
+julia> fibonacci_sequence()
+5-element Vector{Any}:
+ BigInt[1, 1, 2]
+ BigInt[3, 5, 8]
+ BigInt[13, 21, 34]
+ BigInt[55, 89, 144]
+ BigInt[233, 377, 610]
+
+julia> fibonacci_sequence([2], 3, true)
+3-element Vector{Any}:
+ BigInt[1, 3]
+ BigInt[8, 21]
+ BigInt[55, 144]
+
+julia> fibonacci_sequence([2,3], 4)
+4-element Vector{Any}:
+ BigInt[1, 1]
+ BigInt[2, 3, 5]
+ BigInt[8, 13]
+ BigInt[21, 34, 55]
+
 ```
 """
-function random_fibonachi_triple()
-    a, b, c = 0, 1, 1
-    i = 1
-    i_max = rand(1:100)
-    while i<i_max
-        # println(a,' ',b, ' ',c)
-        a = b
-        b = c
-        c = a+b
-        i += 1        
+function fibonacci_sequence(sequence_length_array::Array{Int} = [3], array_number::Integer = 5, get_even::Bool= false)
+    a, b = BigInt(1), BigInt(1)
+    k = 1
+    m = 1
+    result = []
+    while m <= array_number
+        for sequence_length in sequence_length_array
+            sequence = Array{BigInt}(undef, 0)
+            j = 1
+            while j <= sequence_length
+                if !get_even || (get_even && iseven(k))
+                    push!(sequence, a)
+                    j += 1
+                end
+                a_old = a
+                a = b
+                b = a_old + a
+                k += 1
+            end
+            # println(sequence)
+            push!(result, sequence)
+            m += 1
+            if m > array_number
+                break
+            end
+        end
     end
-    return [a,b,c]
+    return result
 end
 
-export random_pair, random_fibonachi_triple
+export random_pair, fibonacci_sequence
+
+# fib(n::Integer) = n ≤ 2 ? one(n) : fib(n-1) + fib(n-2)
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,6 @@ using Test
 @testset "TestPackage.jl" begin
     @test length(TestPackage.random_pair()) == 2
     @test isreal(random_pair()) == true
-    fib_out = TestPackage.random_fibonachi_triple()
-    @test isreal(fib_out) == true
-    @test length(fib_out) == 3    
-    @test fib_out[1]+fib_out[2] == fib_out[3]
+    @test TestPackage.fibonacci_sequence([3],20, true)[end] == BigInt[781774079430987230203437, 2046711111473984623691759, 5358359254990966640871840]
+    @test TestPackage.fibonacci_sequence([3],20)[end] == BigInt[591286729879, 956722026041, 1548008755920]
 end
